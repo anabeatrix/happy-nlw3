aace286bd5f58b8d7a7cae4e353c74ac787ee246
anabeatriz <annabhiaaraujo.silva@gmail.com>
2020-10-16 12:07:39 -0300

    métodos de captura de imagem selecionada e atualiza container

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,46 @@
+const options = {
+  dragging: false,
+  touchZoom: false,
+  doubleClickZoom: false,
+  scrollWheelZoom: false,
+  zoomControl: false,
+};
+
+// create map
+const map = L.map("mapid", options).setView([-10.1853894, -48.3388782], 15);
+
+//create and add tileLayer
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+// create icon
+const icon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
+
+// create and add marker
+L.marker([-10.1853894, -48.3388782], { icon }).addTo(map);
+
+// image gallery
+
+function selectImage(event) {
+  const button = event.currentTarget;
+
+  // remover todas as classes .active
+  const buttons = document.querySelectorAll(".images button");
+  buttons.forEach((button) => {
+    button.classList.remove("active");
+  });
+
+  // adicionar classe .active para botão selecionado
+  button.classList.add("active");
+
+  // selecionar imagem clicada
+  const image = button.children[0];
+  const imageContainer = document.querySelector(".orphanage-details > img");
+
+  // atualizar container de imagem 
+  imageContainer.src = image.src;
+}
